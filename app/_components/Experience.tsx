743b386e132023e37992a7792bcdac3cd315b19c
Jake Pitman <jakeseanp@gmail.com>
2024-08-17 14:26:36 +0900

    Remove leva

--- app/_components/Experience.tsx
+++ app/_components/Experience.tsx
@@ -4,16 +4,12 @@ import { OrbitControls } from "@react-three/drei";
 
 import { BuildingsStrip } from "@components/BuildingsStrip";
 import { Background } from "@components/Background";
-import { useControls } from "leva";
 
 export const Experience = () => {
   const { gl } = useThree();
   // Limit renderer's pixel ratio to 2 for consistency
   gl.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-  const { rotation } = useControls({
-    rotation: [0, Math.PI, 0],
-  });
   const numberOfBuildingRows = 40;
   const numberOfBuildingsPerBlock = 5;
 
